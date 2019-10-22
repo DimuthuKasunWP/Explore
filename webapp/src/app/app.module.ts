61dad32777fd821e93c4b46f6043c1efd3237dfa
DimuthuKasunWP <kasunwpdimuthu@gmail.com>
2019-10-22 12:46:05 +0530

    app.module updated

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
