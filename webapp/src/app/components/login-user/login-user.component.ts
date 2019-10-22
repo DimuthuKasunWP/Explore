cf53f873c98747cec5308811b173a5a31bd67293
DimuthuKasunWP <kasunwpdimuthu@gmail.com>
2019-10-22 13:01:29 +0530

    user-login created

---
+++ webapp/src/app/components/login-user/login-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login-user',
+  templateUrl: './login-user.component.html',
+  styleUrls: ['./login-user.component.css']
+})
+export class LoginUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
