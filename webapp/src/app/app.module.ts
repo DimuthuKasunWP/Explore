1d76035b4e3ccff834b182112e24a2e75b3c796b
DimuthuKasunWP <kasunwpdimuthu@gmail.com>
2019-10-23 13:04:06 +0530

    firebase dependencies addded

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -6,9 +6,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 // Third party
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireStorageModule } from 'angularfire2/storage';
 import { ChartsModule } from 'ng2-charts';
 import {NgxAutoScrollModule} from 'ngx-auto-scroll';
-
+import * as firebase from 'firebase';
 
 
 // Pipes and Directives
@@ -27,38 +31,38 @@ import { LikesService } from './services/likes.service';
 import { NotificationService } from './services/notification.service';
 
 // Components
-import { AppComponent } from '../app/app.component';
-import { HomeComponent } from '../app/components/home/home.component';
-import { NavbarComponent } from '../app/components/navbar/navbar.component';
-import { FooterComponent } from '../app/components/footer/footer.component';
-import { LandingComponent } from '../app/components/landing/landing.component';
-import { LoginComponent } from '../app/components/login/login.component';
-import { RegisterComponent } from '../app/components/register/register.component';
-import { ProfileComponent } from '../app/components/profile/profile.component';
-import { ErrorComponent } from '../app/components/error/error.component';
-import { AccountComponent } from '../app/components/account/account.component';
-import { PostComponent } from '../app/components/post/post.component';
-import { SearchComponent } from '../app/components/search/search.component';
-import { AboutComponent } from '../app/components/about/about.component';
-import { AddPostComponent } from '../app/components/add-post/add-post.component';
-import { UserListComponent } from '../app/components/user-list/user-list.component';
-import { UserComponent } from '../app/components/user-list/user/user.component';
-import { GroupComponent } from '../app/components/group/group.component';
-import { AddCommentComponent } from '../app/components/add-comment/add-comment.component';
-import { CreateGroupComponent } from '../app/components/create-group/create-group.component';
-import { MessagingComponent } from '../app/components/messaging/messaging.component';
-import { ChatroomComponent } from '../app/components/messaging/chatroom/chatroom.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { LandingComponent } from './landing/landing.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ErrorComponent } from './error/error.component';
+import { AccountComponent } from './account/account.component';
+import { PostComponent } from './post/post.component';
+import { SearchComponent } from './search/search.component';
+import { AboutComponent } from './about/about.component';
+import { AddPostComponent } from './add-post/add-post.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserComponent } from './user-list/user/user.component';
+import { GroupComponent } from './group/group.component';
+import { AddCommentComponent } from './add-comment/add-comment.component';
+import { CreateGroupComponent } from './create-group/create-group.component';
+import { MessagingComponent } from './messaging/messaging.component';
+import { ChatroomComponent } from './messaging/chatroom/chatroom.component';
 import { MessageService } from './services/message.service';
-import { ChatroomlistComponent } from '../app/components/messaging/chatroomlist/chatroomlist.component';
-import { MessageComponent } from '../app/components/messaging/chatroom/message/message.component';
-import { SuggestedComponent } from '../app/components/suggested/suggested.component';
-import { FeedbackComponent } from '../app/components/feedback/feedback.component';
-import { AdminComponent } from '../app/components/admin/admin.component';
-import { GrouplistComponent } from '../app/components/group/grouplist/grouplist.component';
-import { NotificationComponent } from '../app/components/notification/notification.component';
+import { ChatroomlistComponent } from './messaging/chatroomlist/chatroomlist.component';
+import { MessageComponent } from './messaging/chatroom/message/message.component';
+import { SuggestedComponent } from './suggested/suggested.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { AdminComponent } from './admin/admin.component';
+import { GrouplistComponent } from './group/grouplist/grouplist.component';
+import { NotificationComponent } from './notification/notification.component';
 
 
-// firebase.initializeApp(environment.firebase);
+firebase.initializeApp(environment.firebase);
 
 const routes: Routes = [
   {
@@ -162,12 +166,12 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
-    // NgbModule.forRoot(),
+    NgbModule.forRoot(),
     FormsModule,
-    // AngularFireModule.initializeApp(environment.firebase, 'Scribe'),
-    // AngularFirestoreModule,
-    // AngularFireAuthModule,
-    // AngularFireStorageModule,
+    AngularFireModule.initializeApp(environment.firebase, 'Scribe'),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
     RouterModule.forRoot(routes),
     ChartsModule,
     ReactiveFormsModule,
