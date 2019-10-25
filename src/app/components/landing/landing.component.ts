c1636ed2ebd37effc9659797f22751c5452e17dd
DimuthuKasunWP <kasunwpdimuthu@gmail.com>
2019-10-25 07:27:26 +0530

    landing component bug fixed

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../services/auth.service';
+import { AuthService } from '../../services/auth.service';
 import { Title } from '@angular/platform-browser';
 
 @Component({
