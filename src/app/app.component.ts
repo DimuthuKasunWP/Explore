1793f85cafff5441eb1e6aa453bee4a27e67db48
RajithaGayashan <rajithagayashan95@gmail.com>
2019-11-18 00:38:20 +0530

    Event sort list added(Calender)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css'],
   })
 export class AppComponent implements OnInit {
+  name = 'Angular';
+  private exportTime = { hour: 7, minute: 15, meriden: 'PM', format: 24 };
 
-  ngOnInit() {
-  }
+  onChangeHour(event) {
+    console.log('event', event);
+  // ngOnInit() {
+  // }
+}
 }
 
