5953045c19d28a564d9013e0c677e762eebbc0a7
Dimuthu <kasunwpdimuthu@gmail.com>
2020-01-14 16:01:33 +0530

    live searching added to the location event with google maps api

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,12 +1,27 @@
-import { Component, OnInit, Inject } from '@angular/core'
+import {Component, OnInit, Inject, ViewChild, ElementRef, NgZone} from '@angular/core';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog'
+import {MapsAPILoader} from '@agm/core';
+// import {google} from '@agm/core/services/google-maps-types';
+// import {} from 'googlemaps';
 @Component({
   selector: 'app-event',
   templateUrl: './event.component.html',
   styleUrls: ['./event.component.css']
 })
 export class EventComponent implements OnInit {
-  constructor () { }
+
+  latitude: number;
+  longitude: number;
+  zoom: number;
+  address: string;
+  private geoCoder;
+
+  @ViewChild('search',{static:false})
+  public searchElementRef: ElementRef;
+
+
+  constructor ( private mapsAPILoader: MapsAPILoader,
+                private ngZone: NgZone) { }
   openDialog () {
     console.log('The dialog was closed')
   }
@@ -25,8 +40,72 @@ export class EventComponent implements OnInit {
   // }
 
   ngOnInit () {
+    this.mapsAPILoader.load().then(() => {
+      this.setCurrentLocation();
+      this.geoCoder = new google.maps.Geocoder;
+
+      let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+        types: ["address"]
+      });
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          //get the place result
+          let place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+          //verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+
+          //set latitude, longitude and zoom
+          this.latitude = place.geometry.location.lat();
+          this.longitude = place.geometry.location.lng();
+          this.zoom = 12;
+        });
+      });
+    });
   }
+
+  private setCurrentLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.latitude = position.coords.latitude;
+        this.longitude = position.coords.longitude;
+        this.zoom = 8;
+        this.getAddress(this.latitude, this.longitude);
+      });
+    }
+  }
+
+
+  // markerDragEnd($event: MouseEvent) {
+  //   console.log($event);
+  //   this.latitude = $event.coords.lat;
+  //   this.longitude = $event.coords.lng;
+  //   this.getAddress(this.latitude, this.longitude);
+  // }
+
+  getAddress(latitude, longitude) {
+    this.geoCoder.geocode({ 'location': { lat: latitude, lng: longitude } }, (results, status) => {
+      console.log(results);
+      console.log(status);
+      if (status === 'OK') {
+        if (results[0]) {
+          this.zoom = 12;
+          this.address = results[0].formatted_address;
+        } else {
+          window.alert('No results found');
+        }
+      } else {
+        window.alert('Geocoder failed due to: ' + status);
+      }
+
+    });
+  }
+
+
 }
+
 
 // @Component({
 //   selector: 'dialog-overview-example-dialog',
