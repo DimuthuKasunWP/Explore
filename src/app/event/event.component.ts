be9f0434d66ba4a9c03b039f1f5137bafd484455
RajithaGayashan <rajithagayashan95@gmail.com>
2020-01-14 10:37:18 +0530

    event

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Inject } from '@angular/core'
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog'
 @Component({
   selector: 'app-event',
   templateUrl: './event.component.html',
   styleUrls: ['./event.component.css']
 })
 export class EventComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor () { }
+  openDialog () {
+    console.log('The dialog was closed')
   }
 
+  // openDialog(): void {
+  //   const dialogRef = MatDialog.open(DialogOverviewExampleDialog, {
+  //     width: '250px',
+  //     data: {name: this.name, animal: this.animal}
+  //   });
+  //   console.log('The dialog was closed');
+
+  //   dialogRef.afterClosed().subscribe(result => {
+  //     console.log('The dialog was closed');
+  //     this.animal = result;
+  //   });
+  // }
+
+  ngOnInit () {
+  }
 }
+
+// @Component({
+//   selector: 'dialog-overview-example-dialog',
+//   templateUrl: 'dialog-overview-example-dialog.html',
+// })
+
+// export class DialogOverviewExampleDialog {
+
+//   constructor(
+//     public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+//     @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+//   onNoClick(): void {
+//     this.dialogRef.close();
+//   }
+
+// }
