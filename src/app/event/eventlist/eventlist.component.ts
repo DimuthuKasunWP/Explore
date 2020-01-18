3c145f1c37a4cb97488ca91720bbb1edeb604ac0
Dimuthu <kasunwpdimuthu@gmail.com>
2020-01-18 21:06:40 +0530

    initialization added

--- src/app/event/eventlist/eventlist.component.ts
+++ src/app/event/eventlist/eventlist.component.ts
@@ -1,8 +1,8 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
-import {UserService} from '../services/user.service';
+import {UserService} from '../../services/user.service';
 import {Title} from '@angular/platform-browser';
-import {EventsService} from '../services/events.service';
+import {EventsService} from '../../services/events.service';
 
 @Component({
   selector: 'app-eventlist',
@@ -10,17 +10,47 @@ import {EventsService} from '../services/events.service';
   styleUrls: ['./eventlist.component.css']
 })
 export class EventlistComponent implements OnInit {
-  @Input()events
+  @Input()events;
   @Input()modalRef;
+
+  username;
   constructor(
 
     private route: ActivatedRoute,
-    private groupService: EventsService,
+    private eventService: EventsService,
     private userService: UserService,
     private titleService: Title
   ) { }
 
   ngOnInit() {
+
+    // if (!this.events) {
+    //   this.route.params.subscribe(router => {
+    //     this.username = router.username;
+    //     this.titleService.setTitle(this.username + '\'s Events')
+    //     this.userService.retrieveUserDocumentFromUsername(this.username).subscribe(currentuser => {
+    //       const curruser: any = currentuser[0];
+    //       this.userService.getUserEvents(curruser.uid).subscribe(userEvents => {
+    //         if (userEvents) {
+    //           this.events = [];
+    //           userEvents.forEach((groupData: any) => {
+    //             this.eventService.getGroup(groupData.gid).subscribe(
+    //               eventDetails => {
+    //                 this.events.push(eventDetails);
+    //               });
+    //           });
+    //         }
+    //       });
+    //     });
+    //   });
+    // }
+
+  }
+
+  close() {
+    if (this.modalRef) {
+      this.modalRef.close();
+    }
   }
 
 }
