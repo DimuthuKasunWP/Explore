8bcf99eb96d098fe8549cbd32e22d9a22ba0eb55
Dimuthu <kasunwpdimuthu@gmail.com>
2020-01-18 19:34:13 +0530

    event list component ng template content added
    And Constructor initialized

--- src/app/eventlist/eventlist.component.ts
+++ src/app/eventlist/eventlist.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {UserService} from '../services/user.service';
+import {Title} from '@angular/platform-browser';
+import {EventsService} from '../services/events.service';
 
 @Component({
   selector: 'app-eventlist',
@@ -6,8 +10,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./eventlist.component.css']
 })
 export class EventlistComponent implements OnInit {
+  @Input()events
+  @Input()modalRef;
+  constructor(
 
-  constructor() { }
+    private route: ActivatedRoute,
+    private groupService: EventsService,
+    private userService: UserService,
+    private titleService: Title
+  ) { }
 
   ngOnInit() {
   }
