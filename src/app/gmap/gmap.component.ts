1da07b5c7828ce45ccba1c0649936ad57db2611c
SachinduHimash <himash1997@gmail.com>
2020-02-14 20:05:10 +0530

    made some changes

--- src/app/gmap/gmap.component.ts
+++ src/app/gmap/gmap.component.ts
@@ -8,8 +8,15 @@ import { BrowserModule } from '@angular/platform-browser';
   styleUrls: ['./gmap.component.css']
 })
 export class GmapComponent implements OnInit {
-    latitude= 51.678418;
-    longitude=7.809007;
+    latitude= 7.8774;
+    longitude=80.7003;
+    locationChosen = false;
+
+  onChoseLocation(event) {
+    this.latitude = event.coords.lat;
+    this.longitude = event.coords.lng;
+    this.locationChosen = true;
+  }
   constructor() { }
 
   ngOnInit() {
