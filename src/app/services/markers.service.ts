52261aca5bf38969c6ae7db29238c5077924df59
SachinduHimash <himash1997@gmail.com>
2020-02-15 16:23:59 +0530

    made changes in the marker form

--- src/app/services/markers.service.ts
+++ src/app/services/markers.service.ts
@@ -1,21 +1,27 @@
 import { Injectable } from '@angular/core';
 import {FormControl,FormGroup, FormControlName,Validators}from '@angular/forms'
 import { from } from 'rxjs';
+import{AngularFireDatabase,AngularFireList} from 'angularfire2/database'
 
 @Injectable({
   providedIn: 'root'
 })
 export class MarkersService {
-  form: any;
+  
 
-  constructor() { 
-      this.form = new FormGroup({
+  constructor(private firebase: new AngularFireDatabase) {}
+      
+  markerslist:AngularFireList<any>;
+
+      form = new FormGroup({
       $key :new FormControl(null),
       markerName: new FormControl('',Validators.required),
       eventID: new FormControl(''),
       location:new FormControl('',Validators.required),
       description:new FormControl(''),
-});
+    });
 
-  }
+    getmarkers(){
+      this.markerslist= this.firebase.list("markers");
+    }
 }
