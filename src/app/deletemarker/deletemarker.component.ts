ef62063db8be0fe24f300d78e68bdd58aab5995f
SachinduHimash <himash1997@gmail.com>
2020-02-17 17:57:17 +0530

    set changes in delete marker component

--- src/app/deletemarker/deletemarker.component.ts
+++ src/app/deletemarker/deletemarker.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { timeout } from 'rxjs/operators';
 
 @Component({
   selector: 'app-deletemarker',
@@ -6,10 +8,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./deletemarker.component.css']
 })
 export class DeletemarkerComponent implements OnInit {
+  coffee;
+  markers;
+  items: any[];
+  
+  
+  constructor(private afs: AngularFirestore ) {}
+  coffees = ["Americano", "Flat White", "Cappuccino", "Latte", "Espresso", "Machiato", "Mocha", "Hot Chocolate", "Tea"];
 
-  constructor() { }
+  
+  addCoffee(coffee){ }
+  
+  async ngOnInit()
+  {
+  var count=0;
+   this.markers= await this.afs.collection("markers").valueChanges().subscribe(
+     val=>{
+       if(val){
+         console.log(val)
+        let i
+        for(i =0;i<val.length;i++){
+          //@ts-ignore
+             this.items.push(val[i].markerName);
+             
+       }
+       
+     } 
 
-  ngOnInit() {
-  }
-
+  });
+  setInterval(() => {
+    console.log(this.items);
+  },20000)
+  
+ 
+  } 
 }
