e226e298965a49ab3158394dc6fea0ba8e6a9541
Dimuthu <kasunwpdimuthu@gmail.com>
2020-02-19 08:34:22 +0530

    reformat delete marker component

--- src/app/deletemarker/deletemarker.component.ts
+++ src/app/deletemarker/deletemarker.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from 'angularfire2/firestore';
-import { timeout } from 'rxjs/operators';
+import {Component, OnInit} from '@angular/core';
+import {AngularFirestore} from 'angularfire2/firestore';
 
 @Component({
   selector: 'app-deletemarker',
@@ -10,58 +9,55 @@ import { timeout } from 'rxjs/operators';
 export class DeletemarkerComponent implements OnInit {
   coffee;
   marker;
-  items=[];
+  items = [];
 
 
-  constructor(private afs: AngularFirestore ) {}
-  
-
-  
-
-  async ngOnInit()
-  {
-  var count=0;
-   this.afs.collection("markers").valueChanges().subscribe(
-     val=>{
-       if(val){
-         // @ts-ignore
-         console.log("marker name"+val[0].markerName);
-
-        while(count<Object.keys(val).length){
-          //@ts-ignore
-             this.items.push(val[count++].markerName);
-
-       }
-         console.log(this.items);
-     }
+  constructor(private afs: AngularFirestore) {
+  }
 
 
+  async ngOnInit() {
+    var count = 0;
+    this.afs.collection('markers').valueChanges().subscribe(
+      val => {
+        if (val) {
+          // @ts-ignore
+          console.log('marker name' + val[0].markerName);
 
-  });
+          while (count < Object.keys(val).length) {
+            //@ts-ignore
+            this.items.push(val[count++].markerName);
 
+          }
+          console.log(this.items);
+        }
+
+
+      });
 
 
   }
+
   async deleteMarker(data) {
     //  console.log("this is markers"+data);
     //  this.afs.collection("markers",ref =>ref.where ('markerName','==',data)).doc().delete();
-    
+
     // this.afs.collection('markers',ref=>ref.where('markerName','==',data)).valueChanges()
-    const docs =  await this.afs.firestore
+    const docs = await this.afs.firestore
       .collection('markers')
       .where('markerName', '==', data)
       .get();
 
-      docs.docs.map((doc)=> {
-        this.afs.firestore
-          .collection('markers')
-          .doc(doc.id)
-          .delete().then(()=> console.log('delete'));
-      });
-    
-      
-    }
-        
- }
+    docs.docs.map((doc) => {
+      this.afs.firestore
+        .collection('markers')
+        .doc(doc.id)
+        .delete().then(() => console.log('delete'));
+    });
+
+
+  }
+
+}
 
 
