4d67164091699139c97692ba9dfa05f3a13d5812
DimuthuKasunWP <kasunwpdimuthu@gmail.com>
2020-02-20 17:56:05 +0530

    remove api keys from the environment .prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyDjOzHVxpB92O3xrmKXYxWfbtklo3jZqio',
-    authDomain: 'xplore-1.firebaseapp.com',
-    databaseURL: 'https://xplore-1.firebaseio.com',
-    projectId: 'xplore-1',
-    storageBucket: 'xplore-1.appspot.com',
-    messagingSenderId: '333841015232',
-    appId: '1:333841015232:web:791ce71c25e29aedfba825'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: ''
   },
-  googleMapsKey: 'AIzaSyD_gQGzi-IDe-KM69qlbUsOmKTvhuIscjg'
+  googleMapsKey: ''
 };
